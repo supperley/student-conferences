f93e0ee092ecf43033208b364b59b6c930a5f3d8
supperley <v.bobrovko@gmail.com>
2024-01-08 17:02:40 +0300

    add react-router-dom

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,14 +1,11 @@
 import Dashboard from '../../components/Dashboard/Dashboard';
-import PageLayout from '../../components/PageLayout/PageLayout';
 import styles from './Home.module.css';
 
 const Home = () => {
   return (
-    <div className={styles.container}>
-      <PageLayout>
-        <Dashboard />
-      </PageLayout>
-    </div>
+    <main className={styles.container}>
+      <Dashboard />
+    </main>
   );
 };
 
---
+++ src/shared/config/routes.ts
@@ -0,0 +1,10 @@
+export const ROUTE_CONSTANTS = {
+  HOME: "/",
+  LOGIN: "/login",
+  REGISTER: "/register",
+  BLOG: "/blog",
+  USERS: "/users",
+  USER: "/user/:userId",
+  SETTINGS: "/settings",
+  NOT_FOUND: "/404",
+};
