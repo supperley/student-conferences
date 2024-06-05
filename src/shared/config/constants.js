89264282665b46e6e96bc0603286a457584dd5c7
undefined <undefined>
2024-06-05 17:37:58 +0300

    add .env.example

--- src/shared/config/constants.js
+++ src/shared/config/constants.js
@@ -1,5 +1,7 @@
-export const BASE_URL = import.meta.env.VITE_ENV
-  ? 'http://localhost:3000'
+export const BASE_URL = import.meta.env.VITE_URL_TO_SERVER
+  ? import.meta.env.VITE_URL_TO_SERVER
   : 'http://localhost:3000';
 
-export const S3_URL = import.meta.env.VITE_ENV ? 'http://localhost:3000' : 'http://localhost:3000';
+export const S3_URL = import.meta.env.VITE_URL_TO_S3
+  ? import.meta.env.VITE_URL_TO_S3
+  : 'http://localhost:3000';
