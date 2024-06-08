16081507e625219852676ad784d1758d9d78f418
undefined <undefined@mail.com>
2024-06-08 10:56:46 +0300

    edit nginx config

--- src/shared/config/constants.js
+++ src/shared/config/constants.js
@@ -1,7 +1,3 @@
-export const BASE_URL = import.meta.env.VITE_URL_TO_SERVER
-  ? import.meta.env.VITE_URL_TO_SERVER
-  : 'http://localhost:3000';
+export const BASE_URL = import.meta.env.DEV ? import.meta.env.VITE_URL_TO_SERVER : '';
 
-export const S3_URL = import.meta.env.VITE_URL_TO_S3
-  ? import.meta.env.VITE_URL_TO_S3
-  : 'http://localhost:3000';
+export const S3_URL = import.meta.env.DEV ? import.meta.env.VITE_URL_TO_S3 : '';
